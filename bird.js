b50ad9f859119b9288a00886c7736d720edade02
YakovKulikov <122715669+YakovKulikov@users.noreply.github.com>
2024-04-28 19:13:51 +1000

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,11 @@
+const handle = () => {
+    const inputs = document.querySelector("input");
+    let arr = [];
+    // for (i=0; i < inputs.length; i++) {
+    //     arr.push(inputs[i].value)
+    // }
+    inputs.forEach(el => arr.push(el.value))
+    alert(arr.join(""));
+}
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", handle);
